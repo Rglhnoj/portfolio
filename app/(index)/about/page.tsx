ad20f7e9b93760f987aa188dda87fd6472c30d12
Rglhnoj <recuerdo@gmail.com>
2026-02-01 15:42:11 +0800

    first day , cleaning the next js then i add the components

---
+++ app/(index)/about/page.tsx
@@ -0,0 +1,5 @@
+export default function AboutPage(){
+    return(
+        <main> About Page </main>
+    )
+}   
---
+++ components/common/footer.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+
+export function Footer() {
+    return (
+        <footer className="border-t border-gray-800 py-5">
+            <div className=" container mx-auto px-6">
+                <div className="flex justify-between">
+                    <div>
+                        <h2 className="font-bold">Portfolio</h2>
+                        <p>Cordova Cebu City</p>
+                    </div>
+
+                    <div>
+                        <h2 className="font-bold mb-4">Quick Links</h2>
+                        <ul className="flex flex-col gap-2">
+                        <li><Link href="/">Home</Link></li>
+                        <li><Link href="/about">About</Link></li>
+                        <li><Link href="/projects">Projects</Link></li>
+                        </ul>         
+                    </div>
+                      <div>
+            <h2 className="font-bold mb-4">Blogs</h2>
+            <ul className="flex flex-col gap-2">
+              <li><Link href="/">Blog 1</Link></li>
+              <li><Link href="/about">Blog 2</Link></li>
+              <li><Link href="/projects">Blog 3</Link></li>
+            </ul>
+          </div>
+
+          {/* Contacts */}
+          <div>
+            <h2 className="font-bold mb-4">Contact Info</h2>
+            <ul className="flex flex-col gap-2">
+              <li>Phone: <Link href="/">+639 93 412 7869</Link></li>
+              <li>Email: <Link href="/about">email@test.com</Link></li>
+              <li><Link href="/projects">Visit Github</Link></li>
+            </ul>
+          </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
