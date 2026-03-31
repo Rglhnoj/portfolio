48fb1cc3651955cfc3740835f7503ebdef60179e
Rglhnoj <recuerdo@gmail.com>
2026-03-31 15:46:40 +0800

    Checking and Adding all the components that i need and identify where i can locate it

--- app/(guest)/(index)/page.tsx
+++ app/(guest)/(index)/page.tsx
@@ -1,8 +1,15 @@
-export default function HomePage(){
-    return(
-        <main>
-            Home Page
-        
-        </main>
-    )
+import { HomeBanner } from "@/components/features/home/HomeBanner";
+import { Projects } from "@/components/features/home/Project";
+import { Skills } from "@/components/features/home/Skills";
+import { Contact } from "@/components/features/home/Contact";
+
+export default function HomePage() {
+  return (
+    <>
+      <HomeBanner />
+      <Projects />
+      <Skills />
+      <Contact />
+    </>
+  )
 }
