7608a46e9edef95d8701c5d05717c51c20fcd64e
Rglhnoj <recuerdo@gmail.com>
2026-04-01 15:17:47 +0800

    Adjusting to become scrollable and adding the about me page

--- app/(guest)/layout.tsx
+++ app/(guest)/layout.tsx
@@ -1,13 +1,16 @@
 import { Header } from "@/components/common/Header";
 import { Footer } from "@/components/common/Footer";
 
-export default function GuestLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function GuestLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col h-screen">
+    
+    <div className="flex flex-col min-h-screen bg-[#F8FAFC]">
       <Header />
-      <main className="flex-1 container mx-auto px-6">
+      
+      <main className="flex-1 w-full"> 
         {children}
       </main>
+
       <Footer />
     </div>
   );
